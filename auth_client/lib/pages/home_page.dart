9747b4d04374b7e4b1db556cbb4ac94ed4eaa035
ihnokim <ihnokim58@gmail.com>
2022-11-15 20:45:44 +0900

    가입하자마자 User Info 적재되기 전 Fetch 시도해서 발생하는 오류 수정
    - FutureBuilder를 StreamBuilder로 수정해서 실시간으로 상태 얻어오도록 변경

--- auth_client/lib/pages/home_page.dart
+++ auth_client/lib/pages/home_page.dart
@@ -12,50 +12,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
-  Future<DocumentSnapshot<Map<String, dynamic>>> fetchUserInfo() async {
-    final user = FirebaseAuth.instance.currentUser;
-    return FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
-    // print(userInfo['username']);
-    // username = userInfo['username'];
-    // imageUrl = userInfo['image_url'];
-  }
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
+    final Stream<DocumentSnapshot<Map<String, dynamic>>> stream =
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .snapshots();
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const Center(child: Text('로그인 완료')),
-          FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-              future: fetchUserInfo(),
-              builder: (ctx, ss) {
-                if (ss.connectionState == ConnectionState.done) {
-                  var username = ss.data!['username'];
-                  var imageUrl = ss.data!['image_url'];
-                  return Center(
-                    child: Column(
-                      children: [
-                        UserImage(image: NetworkImage(imageUrl)),
-                        Text(_firebaseAuth.currentUser!.email!),
-                        Text(username),
-                      ],
-                    ),
-                  );
-                } else if (ss.hasError) {
-                  throw ss.error!;
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              }),
+          StreamBuilder<DocumentSnapshot>(
+            stream: stream,
+            builder: (ctx, ss) {
+              if (ss.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (ss.hasError) {
+                // throw ss.error!;
+                return Center(
+                  child: Column(
+                    children: [
+                      const UserImage(image: null),
+                      Text(_auth.currentUser!.email!),
+                      const Text('unknown'),
+                    ],
+                  ),
+                );
+              } else if (ss.hasData) {
+                dynamic data = ss.data!.data();
+                return Center(
+                  child: Column(
+                    children: [
+                      UserImage(
+                          image: data == null
+                              ? null
+                              : NetworkImage(data['image_url'])),
+                      Text(_auth.currentUser!.email!),
+                      Text(data == null ? 'unknown' : data['username']),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
           Padding(
             padding: const EdgeInsets.all(10),
             child: ElevatedButton(
               onPressed: () {
-                _firebaseAuth.signOut();
+                _auth.signOut();
               },
               child: const Text('로그아웃'),
             ),
